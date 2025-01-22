11eaadd91e2a4b5966581dea98a152c8d0f47288
Adam Snyder <armsnyder@gmail.com>
2025-01-22 11:25:59 -0800

    feat: Open mini to current file

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -43,7 +43,12 @@ return {
           use_as_default_explorer = false, -- We want to open Telescope instead
         },
       })
-      vim.keymap.set("n", "<leader>e", minifiles.open, { desc = "Open file [E]xplorer" })
+      vim.keymap.set("n", "<leader>e", function()
+        local buf_name = vim.api.nvim_buf_get_name(0)
+        local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
+        minifiles.open(path)
+        minifiles.reveal_cwd()
+      end, { desc = "Open file [E]xplorer" })
 
       vim.api.nvim_create_autocmd("User", {
         pattern = "MiniFilesBufferCreate",
