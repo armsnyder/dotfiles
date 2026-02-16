bc961b2de17e032cec7388d1a1a6b411e293dbea
Adam Snyder <armsnyder@gmail.com>
2026-02-16 14:10:42 -0800

    fix(nvim): Replace deprecated godot lsp setup

--- nvim/.config/nvim/plugin/godot.lua
+++ nvim/.config/nvim/plugin/godot.lua
@@ -4,7 +4,5 @@ if vim.uv.fs_stat(cwd .. "/project.godot") then
   if not vim.uv.fs_stat(cwd .. "/server.pipe") then
     vim.fn.serverstart(cwd .. "/server.pipe")
   end
-
-  local lspconfig = require("lspconfig")
-  lspconfig.gdscript.setup({})
+  vim.lsp.enable("gdscript")
 end
